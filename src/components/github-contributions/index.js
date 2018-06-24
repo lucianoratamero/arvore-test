49d4623653ae35bd030cced500a2a49b8b26054f
Luciano Ratamero <luciano@ratamero.com>
2018-06-23 23:25:56 -0300

    uses formatter to print all necessary data.

--- src/components/github-contributions/index.js
+++ src/components/github-contributions/index.js
@@ -1,15 +1,31 @@
 
 import React from "react";
 
-import contributionsData from '../../data/teste_arvore_data';
+import contributionsData from '../../data/contributions';
+import ContributionsFormatter from './formatter';
 import './github-contributions.scss';
 
 
 export default class GithubContributions extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { data: new ContributionsFormatter(contributionsData) };
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <div id="github-contributions">
-        {JSON.stringify(contributionsData)}
+        <p>Period: {data.period}</p>
+        <p>Max: {data.max}</p>
+        <p>Min: {data.min}</p>
+        <p>Total: {data.total}</p>
+        {data.items.map(contributions => (
+          <p key={contributions.date}>{contributions.date} - {contributions.count} - {contributions.color}</p>
+        ))}
       </div>
     )
   }
